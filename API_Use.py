61d2ea6ba7f0d08ffb49591c36a44097a4169be8
Ruben Glatt <github.rg@gmail.com>
2016-11-06 02:17:10 -0200

    Modified product

--- API_Use.py
+++ API_Use.py
@@ -24,18 +24,19 @@ personality_insights = PersonalityInsightsV3(
     username='1047ef84-6f8b-4657-98c8-a25190589099',
     password='6wt4CYpxpEzp')
 
+
 def get_insight(json_dict):
     profile = personality_insights.profile(
         json_dict, content_type='application/json',
         raw_scores=True, consumption_preferences=True)
-
     print(json.dumps(profile, indent=2))
 
-def get_insight_file(json_file = './resources/personality-v3.json'):
+
+def get_insight_file(json_file='./resources/personality-v3.json'):
     with open(json_file) as profile_json:
         get_insight(profile_json.read())
 
 if __name__ == "__main__":
-    json_example = generator_api.get_list_text_amazon(UserID = 1234)
+    json_example = generator_api.get_list_text_amazon(UserID=1234)
     get_insight(json_example)
-    #get_insight_file()
+    # get_insight_file()
